b4d5605af374bcb571b3ea47482a7056ce2cab40
Sumit Oberoi <soberoi@Administrators-MacBook-Pro.local>
2019-05-20 20:19:19 +0100

    Pushing to git

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sign in with Apple in SwiftUI
+New in iOS 13 is Sign in With Apple
+Here is my implementation of it
+
+
+Follow the setup instruction here: https://help.apple.com/developer-account/#/devde676e696
+
+<img src="https://j.gifs.com/mOEAyR.gif" width="350" alt="Sign in with Apple SwiftUI component in action">
---
+++ iOS-Demo-SignInWithApple-SwiftUI/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  iOS-Demo-SignInWithApple-SwiftUI
+//
+//  Created by Sumit Oberoi on 05/06/2019.
+//  Copyright © 2019 Sumit Oberoi. All rights reserved.
+//
+
+import SwiftUI
+
+extension CredentialsOrError {
+  var isSuccess: Bool {
+    switch self {
+    case .credentials: return true
+    case .error: return false
+    }
+  }
+  
+  var values: (user: String, givenName: String?, familyName: String?, email: String?)? {
+    switch self {
+    case let .credentials(user: user, givenName: givenName, familyName: familyName, email: email): return (user: user, givenName: givenName, familyName: familyName, email: email)
+    case .error: return nil
+    }
+  }
+  
+  var error: Error? {
+    switch self {
+    case .credentials: return nil
+    case .error(let error): return error
+    }
+  }
+}
+
+struct ContentView : View {
+  @State var credentials: CredentialsOrError?
+  
+  var body: some View {
+    VStack {
+      if $credentials.value == nil {
+        SignInWithAppleButton(credentials: $credentials)
+      } else if $credentials.value!.isSuccess
+      {
+        Text("User: \($credentials.value!.values!.user)")
+        Text("Given name: \($credentials.value!.values?.givenName ?? "")")
+        Text("Family name: \($credentials.value!.values?.familyName ?? "")")
+        Text("Email: \($credentials.value!.values?.email  ?? "")")
+      } else {
+        Text($credentials.value!.error!.localizedDescription)
+          .color(.red)
+      }
+    }
+  }
+}
